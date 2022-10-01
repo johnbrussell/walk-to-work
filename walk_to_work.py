b45233a83f8e682caa5c9f90a10910d3f3e0c974
JBR <john.russell@tufts.edu>
2022-10-01 14:38:19 -0400

    Don't try to prune eliminations dict

--- walk_to_work.py
+++ walk_to_work.py
@@ -254,22 +254,9 @@ def fill_out_choice_results_dict(choice_id, choice_results_dict, decision_dict, 
         all_results = {key: result for key, result in choice_results_dict[tpe].items() if
                        key.startswith(f"{choice_id}:")}
         eliminated_results = [key for key, result in all_results.items() if (result.min_wait > new_result.max_wait or
-                              (result.min_wait > new_result.min_wait and result.max_wait > new_result.max_wait and
+                              (result.min_wait >= new_result.min_wait and result.max_wait >= new_result.max_wait and
                                result.avg_wait > new_result.avg_wait)) and key not in eliminations_list[tpe][choice_id]]
         eliminations_list[tpe][choice_id].extend(eliminated_results)
-
-
-def prune_eliminations_list(eliminations_list, decision_dict):
-    for tpe, choice_dict in eliminations_list.items():
-        choice_ids = [str(d) for d in sorted([int(c) for c in choice_dict.keys() if c not in [START, END]],
-                                             reverse=True)]
-        for choice_id in choice_ids:
-            parents = []
-            for c_id, choices in decision_dict.items():
-                if any([[possibility.next_node == choice_id for possibility in c.possibilities] for c in choices]):
-                    parents.append(c_id)
-            if all([eliminations_list[tpe][parent] for parent in parents]):
-                eliminations_list[tpe][choice_id] = []
 
 
 def main():
@@ -299,10 +286,9 @@ def main():
             print(key, result)
 
     print("Eliminations:")
-    prune_eliminations_list(eliminations_list, decision_dict)
     for tpe, choice_dict in eliminations_list.items():
         print(tpe)
-        for choice_id, eliminations in choice_dict.items():
+        for choice_id, eliminations in sorted(choice_dict.items(), key=lambda x: int(x[0]) if x[1] else 0):
             if eliminations:
                 print(choice_id, eliminations)
 
